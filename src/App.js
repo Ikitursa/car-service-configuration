3bc756cc5d2eeaba147c32dd8a5d27d07a2072a2
Maja <pedic.maja@gmail.com>
2022-02-14 20:39:48 +0100

    Adds styling

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ function App() {
       <div className="App">
 
         <header>
-          <div className="header-wrapper">
+          <div className="header-wrapper content-wrapper">
             <div className="logo">TOKIĆ</div>
             <div className="header-text">
-              <h2 className="header-title">Konfigurator servisa</h2>
+              <h3 className="header-title">Konfigurator servisa</h3>
               <p>Izračunajte trošak sevisa</p>
             </div>
           </div>
@@ -21,8 +21,8 @@ function App() {
 
           <div className="config-dialogue">
             <div className="dialogue-header">
-              <h3 className="dialogue-title">Konfigurator servisa</h3>
-              <button className="close">X</button>
+              <h2 className="dialogue-title">Konfigurator servisa</h2>
+              <button className="button-rounded button-secondary close">X</button>
             </div>
 
             {/* Dialogue components*/}
@@ -38,7 +38,7 @@ function App() {
 
                 <div className="dialogue-page-content">
 
-                  <div className="form-wrapper">
+                  <div className="form-wrapper car-brands">
                     <div className="input-wrapper">
                       <input type="radio" id="peugeot" name="brand" value="Peugeot"/>
                       <label htmlFor="peugeot">Peugeot</label>
@@ -51,6 +51,7 @@ function App() {
                       <input type="radio" id="citroen" name="brand" value="Citroen"/>
                       <label htmlFor="citroen">Citroen</label>
                     </div>
+
                   </div>
 
 
@@ -66,7 +67,7 @@ function App() {
 
                 <div className="dialogue-page-content">
 
-                  <div className="form-wrapper">
+                  <div className="form-wrapper services">
                     <div className="input-wrapper">
                       <input type="checkbox" id="service1" name="service"/>
                       <label htmlFor="service1">Zamjena ulja i filtera (500kn)</label>
@@ -83,18 +84,19 @@ function App() {
 
                   <div className="price-wrapper">
                     <div className="coupon-wrapper">
-                      <button>Imam kupon</button>
+                      <button className="button-rounded">Imam kupon</button>
                       <div className="input-wrapper coupon">
                         <input type="text"/>
-                        <button>Primjeni</button>
+                        <button className="button-rounded">Primjeni</button>
                       </div>
-                      <p className="coupon-success">Hvala vam, unjeli ste ispravan kod kupona</p>
+                      <p className="coupon-message success">Hvala vam, unjeli ste ispravan kod kupona</p>
+                      <p className="coupon-message failure">Unjeli ste neispravan kupon, pokušajte ponovo</p>
                     </div>
 
                     <div className="amount">
-                      <p className="base-amount">OSNOVICA:<strong>950,00 KN</strong></p>
-                      <p className="discount-amount">Popust (30%):<strong>-285,00 KN</strong></p>
-                      <p className="total-amount">UKUPNO:<strong>665,00 KN</strong></p>
+                      <p className="base-amount">OSNOVICA: <strong>950,00 KN</strong></p>
+                      <p className="discount-amount">Popust (30%): <strong>-285,00 KN</strong></p>
+                      <p className="total-amount">UKUPNO: <strong>665,00 KN</strong></p>
                     </div>
                   </div>
 
@@ -124,7 +126,7 @@ function App() {
                     </div>
                     <div className="input-wrapper">
                      <textarea id="story" placeholder="Napomena (opcionalno)"
-                               rows="5" cols="33"/>
+                               rows="5"/>
                     </div>
                   </div>
 
@@ -150,7 +152,7 @@ function App() {
                     <div className="summary-item">
                       <div className="item-header">
                         <h2 className="item-title">MODEL VOZILA</h2>
-                        <button>Uredi</button>
+                        <button className="button-rounded outline">Uredi</button>
                       </div>
                       <div className="item-content">
                         <p>Volkswagen</p>
@@ -160,29 +162,29 @@ function App() {
                     <div className="summary-item">
                       <div className="item-header">
                         <h2 className="item-title">ODABRANE USLUGE</h2>
-                        <button>Uredi</button>
+                        <button className="button-rounded outline">Uredi</button>
                       </div>
                       <div className="item-content">
-                        <div className="service-item"><p>Zamjena ulja i filtera</p><p>500 KN</p></div>
-                        <div className="service-item"><p>Promjena pakni</p><p>450 KN</p></div>
+                        <div className="item-row"><p>Zamjena ulja i filtera</p><p>500 KN</p></div>
+                        <div className="item-row"><p>Promjena pakni</p><p>450 KN</p></div>
                         <p>Popust(30%): -285,00 KN</p>
                         <p>UKUPNO: <strong>665,00 KN</strong></p>
                       </div>
                     </div>
 
-                    <div className="summary-item">
+                    <div className="summary-item contact">
                       <div className="item-header">
                         <h2 className="item-title">KONTAKT PODACI</h2>
-                        <button>Uredi</button>
+                        <button className="button-rounded outline">Uredi</button>
                       </div>
                       <div className="item-content">
-                        <div>
-                          <div className="contact-item"><p>Ime i prezime:</p><p>Kristijan Drača</p></div>
-                          <div className="contact-item"><p>Broj telefona:</p><p>2456742572</p></div>
+                        <div className="item-column">
+                          <div className="item-row"><p>Ime i prezime:</p><p>Kristijan Drača</p></div>
+                          <div className="item-row"><p>Broj telefona:</p><p>2456742572</p></div>
                         </div>
-                        <div>
-                          <div className="contact-item"><p>Email adresa:</p><p>kristijan@borealis.biz</p></div>
-                          <div className="contact-item"><p>Napomena:</p><p>Kod auta se prilikom prelaska preko rupa često
+                        <div className="item-column">
+                          <div className="item-row"><p>Email adresa:</p><p>kristijan@borealis.biz</p></div>
+                          <div className="item-row"><p>Napomena:</p><p>Kod auta se prilikom prelaska preko rupa često
                             čuje lupanje pa bi i to trebalo pogledati</p></div>
                         </div>
                       </div>
@@ -195,7 +197,7 @@ function App() {
 
 
               {/* PAGE 5 - AFTER SUBMIT */}
-              <div className="dialogue-page">
+
                 <div className="submit-message">
                   <h3 className="dialogue-page-title">
                     Vaša prijava je uspješno poslana
@@ -204,22 +206,22 @@ function App() {
                     Vaša prijava je uspješno poslana i zaprimljena. Kontaktirati ćemo vas u najkraćem mogućem roku.
                     Hvala vam.
                   </p>
-                  <button>Zatvori</button>
+                  <button className="button-rounded">Zatvori</button>
                 </div>
 
                 <div className="dialogue-page-content">
 
                 </div>
-              </div>
+
 
 
             </div>
             {/* /Dialogue components*/}
 
             <div className="dialogue-controls">
-              <button className="controls">Nazad</button>
-              <button className="controls">Dalje</button>
-              <button className="controls submit">Pošalji</button>
+              <button className="button-rounded">Nazad</button>
+              <button className="button-rounded">Dalje</button>
+              <button className="button-rounded button-secondary">Pošalji</button>
             </div>
 
 
@@ -230,7 +232,7 @@ function App() {
 
           <div className="start-config">
             <p>pritisnite gumb niže kako biste pokrenuli konfigurator</p>
-            <button>Pokreni konfigurator</button>
+            <button className="button-rounded">Pokreni konfigurator</button>
           </div>
         </div>
 
